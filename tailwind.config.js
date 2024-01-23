1921e410ee6e09fd99f4e752ac64aa0a2e9cf4da
masud <masudranaacb@gmail.com>
2024-01-23 11:50:09 +0600

    basic project setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+//   customize tailwind
+//   tailwind.config = {
+//     theme: {
+//       extend: {
+//         colors: {
+//           primary_color: '#000',
+//         }
+//       }
+//     }
+//   }
